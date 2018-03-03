566b30853ff7c5c10f518ce88ce2c337df08cc29
Kirstin Burns <krburns@mtu.edu>
2018-03-03 15:07:06 -0500

    added main menu scene
    The main menu scene has 3 buttons, start, settings, and quit. settings and play lead to other scenes. The settings scene is empty as of right now.

---
+++ Assets/Scripts/MainMenuButtonActions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenuButtonActions : MonoBehaviour {
+
+    // play button, back button, and settings button all load other scenes
+    public void LoadGameScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    //Quit button
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
