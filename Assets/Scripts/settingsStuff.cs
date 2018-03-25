15bad25daa0fef6ae12e1846c96d1f8c5a0394fb
Kirstin Burns <krburns@mtu.edu>
2018-03-25 16:08:13 -0400

    Added settings menu scene
    the new scene has a slider to control the volume of the game and a toggle to get fullscreen. very basic no background yet or anything fancy

---
+++ Assets/Scripts/settingsStuff.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class settingsStuff : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+
+	public void  setVolume( float vol )
+    {
+        audioMixer.SetFloat("volume", vol);
+    }
+
+    public void setFullscreen( bool isFull)
+    {
+        Screen.fullScreen = isFull;
+    }
+}
