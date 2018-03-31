64caa0aad7ea4bfd9e6971ee03cefbf86c9d2f03
Kirstin Burns <krburns@mtu.edu>
2018-03-31 13:57:25 -0400

    changed main screen
    There were so few settings that I got rid of the settings scene and moved the volume control and fullscreen toggle to the main menu.

--- Assets/Scripts/MainMenuButtonActions.cs
+++ Assets/Scripts/MainMenuButtonActions.cs
@@ -1,7 +1,22 @@
 ﻿using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class MainMenuButtonActions : MonoBehaviour {
+
+    // setting volume
+    public AudioMixer audioMixer;
+
+    public void setVolume(float vol)
+    {
+        audioMixer.SetFloat("volume", vol);
+    }
+
+    // fullscreen toggle
+    public void setFullscreen(bool isFull)
+    {
+        Screen.fullScreen = isFull;
+    }
 
     // play button, back button, and settings button all load other scenes
     public void LoadGameScene(string name)
