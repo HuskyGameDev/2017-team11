52821e71a51e5b189d5d297f336fbfd8dafe0bce
Christian LaCourt <cllacour@mtu.edu>
2018-04-14 14:38:56 -0400

    Reordered main menu canvas and added button action script

---
+++ Assets/Scripts/MainMenuUi.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUi : MonoBehaviour {
+    public void ActionStart()
+    {
+        SceneManager.LoadScene("CombatScene");
+    }
+
+    public void ActionQuit()
+    {
+        Application.Quit();
+    }
+}
